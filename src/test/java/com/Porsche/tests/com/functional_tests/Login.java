c52a5b1f945433bc1b8c2671d525977d903a4341
BediaGuvenc48 <43507793+BediaGuvenc48@users.noreply.github.com>
2019-02-18 17:12:40 -0500

    added

--- src/test/java/com/Porsche/tests/com/functional_tests/Login.java
+++ src/test/java/com/Porsche/tests/com/functional_tests/Login.java
@@ -1,4 +1,11 @@
 package com.Porsche.tests.com.functional_tests;
 
 public class Login {
+  
+     Scenario: two tabs
+        Given there are available spots for scheduling
+        And the team lead pull up all the available room for certain time
+        And the user opens the application on a new tab
+        When the user makes a reservation for the same time
+        Then the user should not able able to finalize reservation on the first tab
 }
